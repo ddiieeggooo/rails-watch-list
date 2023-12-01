7175752ea8d23acd5d31af6a7c714ef06bf2489d
ddiieeggoo b <zkpgmi@gmail.com>
2023-12-01 12:38:03 +0100

    image on index

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+  # before_action :set_list, only: [:new, :create]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @bookmarks = Bookmark.all
+    @movies = Movie.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  # @bookmark = Bookmark.new
+  # ou alors
+  # @bookmark = @list.bookmarks.create(bookmark_params)
+
+  def create
+    # @bookmark = Bookmark.new(bookmark_parms)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+   private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+
+
+  # def set_list
+  #   @list = List.find(params[:list_id]) || List.create
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,17 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+    # As a user, I can see all my movie lists ok
+    # As a user, I can create a movie list ok
+    # As a user, I can see the details of a movie list ok
+    # As a user, I can bookmark a movie inside a movie list
+    # dans la show d'une list, je peux create a bookmark
+    # As a user, I can destroy a bookmark
